274689b0b1a9cdf13f33e4ea0d3d7f9deda077d1
Bernard Kastrati <berni.kastrati@hotmail.com>
2023-12-13 21:17:18 +0100

    feat: start working on post page

--- components/post/PostDetails.tsx
+++ components/post/PostDetails.tsx
@@ -9,7 +9,10 @@ import {
   Smartphone,
   TrainFront,
 } from 'lucide-react';
+import Image from 'next/image';
 import React from 'react';
+
+import photo from '@/assets/howitworks/matching.png';
 
 const PostDetails = () => {
   const icons = [
@@ -42,64 +45,30 @@ const PostDetails = () => {
   return (
     <div className='mx-2 my-5 lg:mx-[15%]'>
       <div>posts/pristina</div>
-      <div className='flex flex-row justify-center gap-5'>
-        <div className='border-[1px] rounded-md w-[600px] p-2'>
-          <div className='flex flex-row justify-between items-center border-b-[1px] pb-4'>
-            <h1 className='text-3xl font-noto-sans font-bold'>Iphone 15</h1>
-            <div className='flex gap-2 items-center'>
-              <label className='bg-green-500 text-white p-1 rounded-md font-noto-sans'>
-                Found
-              </label>
-              <label className='font-noto-sans '>27/12/2023</label>
-            </div>
-          </div>
-          <div className='my-5 border-b-[1px] pb-2'>
-            <div className='mb-2 text-lg font-noto-sans font-semibold'>
-              Information
-            </div>
-            <div className='flex flex-col gap-2'>
-              {icons.map(item => (
-                <div className='flex flex-row items-center gap-1' key={item.id}>
-                  <p className='text-orange-500'>{item.icon}</p>
-                  <p className='font-noto-sans'>044314278</p>
+      <div className='flex flex-col justify-center items-center '>
+        <div className='border-[1px] rounded-md p-5 w-[800px] flex flex-col gap-5'>
+          <div className='flex flex-row justify-between'>
+            <div className='flex flex-row gap-5'>
+              <div>
+                <Image width={400} height={400} src={photo} alt='' />
+              </div>
+              <div className='flex flex-col gap-4'>
+                <div>Found: 23/12/2023</div>
+                <div>
+                  <h1>Iphone 15</h1>
                 </div>
-              ))}
+                <div className='flex flex-row gap-1'>
+                  <MapPin />
+                  <p>Podujeve</p>
+                </div>
+              </div>
             </div>
+            <div>1 day</div>
           </div>
-          <div className='border-b-[1px] pb-2 my-5'>
-            <h1 className='mb-2 text-lg font-noto-sans font-semibold'>
-              Airoport
-            </h1>
-            <div className='flex flex-row gap-1 mb-1'>
-              <p>
-                <Plane className='text-orange-500' />
-              </p>
-              <p>Prishtine</p>
-            </div>
-            <div>
-              <p>Number flight: </p>
-            </div>
-          </div>
-          <div className='border-b-[1px] pb-2 my-5'>
-            <h1 className='mb-2 text-lg font-noto-sans font-semibold'>Buss</h1>
-            <div className='flex flex-row gap-1'>
-              <p>
-                <Bus className='text-orange-500' />
-              </p>
-              <p> 116</p>
-            </div>
-          </div>
-          <div className=' my-5'>
-            <h1 className='mb-2 text-lg font-noto-sans font-semibold'>Train</h1>
-            <div className='flex flex-row gap-1'>
-              <p>
-                <TrainFront className='text-orange-500' />
-              </p>
-              <p> 116</p>
-            </div>
+          <div>
+            <h1>hello</h1>
           </div>
         </div>
-        <div className=''>right</div>
       </div>
     </div>
   );
