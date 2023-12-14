a638daabc0d2f890e2caceb4c71cd88418fe46b8
Bernard Kastrati <berni.kastrati@hotmail.com>
2023-12-14 21:46:36 +0100

    feaat: Added postDetails design

--- components/post/PostDetails.tsx
+++ components/post/PostDetails.tsx
@@ -2,12 +2,14 @@ import { foundPosts, lostPosts } from '@/dummyData';
 import {
   Bus,
   Flag,
+  LocateFixed,
   MailCheck,
   MapPin,
   MessageSquare,
   Plane,
   Smartphone,
   TrainFront,
+  UserRound,
 } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react';
@@ -16,6 +18,10 @@ import photo from '@/assets/howitworks/matching.png';
 
 const PostDetails = () => {
   const icons = [
+    {
+      id: 1,
+      icons: <UserRound />,
+    },
     {
       id: 1,
       icon: <Smartphone />,
@@ -48,25 +54,82 @@ const PostDetails = () => {
       <div className='flex flex-col justify-center items-center '>
         <div className='border-[1px] rounded-md p-5 w-[800px] flex flex-col gap-5'>
           <div className='flex flex-row justify-between'>
-            <div className='flex flex-row gap-5'>
+            <div className='flex flex-row gap-5 '>
               <div>
                 <Image width={400} height={400} src={photo} alt='' />
               </div>
-              <div className='flex flex-col gap-4'>
-                <div>Found: 23/12/2023</div>
+              <div className='flex flex-col gap-5'>
+                <div className='flex flex-row items-center gap-1'>
+                  <p className='bg-green-500 p-1 rounded-md text-white font-noto-sans'>
+                    Found:
+                  </p>
+                  <p className='font-noto-sans font-semibold'>23/12/2023</p>
+                </div>
                 <div>
-                  <h1>Iphone 15</h1>
+                  <h1 className='text-3xl font-noto-sans font-semibold'>
+                    Iphone 15
+                  </h1>
+                </div>
+                <div className='flex flex-row gap-1 border-b-[1px] pb-2 text-slate-700 font-noto-sans'>
+                  <MapPin size={22} />
+                  <p>Podujeve</p>
                 </div>
                 <div className='flex flex-row gap-1'>
-                  <MapPin />
-                  <p>Podujeve</p>
+                  <LocateFixed className='text-slate-700' size={22} />
+                  <div>
+                    <p className='text-slate-700 font-noto-sans text-[16px]'>
+                      Exact location
+                    </p>
+                    <p className='text-slate-900 font-noto-sans'>
+                      Afer bankes te raifaisen
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div>1 day</div>
+            <div>
+              <p className='text-sm text-slate-500'>1 day</p>
+            </div>
+          </div>
+          <div className='flex flex-row flex-wrap gap-5 w-auto lg:w-[500px] border-[1px] rounded-md p-2'>
+            <div className='flex flex-row gap-1 flex-wrap'>
+              <UserRound className='text-orange-500 ' size={22} />
+              <div className='flex flex-col gap-1'>
+                <p className=' text-slate-500 font-noto-sans'>Registered</p>
+                <p className='font-noto-sans'>Bernard Kastrati</p>
+              </div>
+            </div>
+            <div className='flex flex-row gap-1 flex-wrap'>
+              <MapPin className='text-orange-500 ' size={22} />
+              <div className='flex flex-col gap-1'>
+                <p className=' text-slate-500 font-noto-sans'>City</p>
+                <p className='font-noto-sans'>Podujeva</p>
+              </div>
+            </div>
+            <div className='flex flex-row gap-1 flex-wrap'>
+              <MapPin className='text-orange-500 ' size={22} />
+              <div className='flex flex-col gap-1'>
+                <p className=' text-slate-500 font-noto-sans'>City</p>
+                <p className='font-noto-sans'>Podujeva</p>
+              </div>
+            </div>
+            <div className='flex flex-row gap-1 flex-wrap'>
+              <MapPin className='text-orange-500 ' size={22} />
+              <div className='flex flex-col gap-1'>
+                <p className=' text-slate-500 font-noto-sans'>City</p>
+                <p className='font-noto-sans'>Podujeva</p>
+              </div>
+            </div>
+            <div className='flex flex-row gap-1 flex-wrap'>
+              <MapPin className='text-orange-500 ' size={22} />
+              <div className='flex flex-col gap-1'>
+                <p className=' text-slate-500 font-noto-sans'>City</p>
+                <p className='font-noto-sans'>Podujeva</p>
+              </div>
+            </div>
           </div>
           <div>
-            <h1>hello</h1>
+            <p>Description</p>
           </div>
         </div>
       </div>
