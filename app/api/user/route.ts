fddaeed6b652872b4eebc8b8e3280a3da8c639e2
Bernard Kastrati <berni.kastrati@hotmail.com>
2024-01-02 15:13:06 +0100

    feat: added auth and database

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,22 +1,22 @@
-import { NextResponse } from 'next/server';
-import { prisma } from '@/server/db';
+// import { NextResponse } from 'next/server';
+// import { prisma } from '@/server/db';
 
-export async function POST(req: Request) {
-  const body = await req.json();
-  const { id, username, name, lastname, password } = body;
-  try {
-    const userPost = await prisma.user.create({
-      data: {
-        id,
-        // username,
-        name,
-        lastname,
-        password,
-      },
-    });
-    return NextResponse.json({ data: userPost });
-  } catch (error) {
-    console.log('Error creating entry', error);
-    return NextResponse.error();
-  }
-}
+// export async function POST(req: Request) {
+//   const body = await req.json();
+//   const { id, username, name, lastname, password } = body;
+//   try {
+//     const userPost = await prisma.user.create({
+//       data: {
+//         id,
+//         // username,
+//         name,
+//         lastname,
+//         password,
+//       },
+//     });
+//     return NextResponse.json({ data: userPost });
+//   } catch (error) {
+//     console.log('Error creating entry', error);
+//     return NextResponse.error();
+//   }
+// }
