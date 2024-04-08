e32eff5d5fd6b0f3e18176ff6e28d8f4a5a3fa6e
Bernard Kastrati <berni.kastrati@hotmail.com>
2024-04-08 20:29:46 +0200

    fix: some functionality for search

--- lib/action.ts
+++ lib/action.ts
@@ -9,7 +9,7 @@ import { auth } from '@clerk/nextjs';
 import axios from 'axios';
 
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION || '',
+  region: process.env.AWS_REGION || 'us-east-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
