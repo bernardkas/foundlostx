45dbc68be7ca029c97569aea912967d89b891f6f
Bernard Kastrati <berni.kastrati@hotmail.com>
2024-04-13 18:59:53 +0200

    fix: error

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: req => !req.url.includes('/foundlost'),
+  publicRoutes: req =>
+    !req.url.includes('/foundlost') || req.url === '/sign-in',
   ignoredRoutes: ['/api/webhook'],
 });
 
-// export const config = {
-//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
