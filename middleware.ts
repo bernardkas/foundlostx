eeadcc7b7d4fabd306528bb90d457dfeaacc2560
Bernard Kastrati <berni.kastrati@hotmail.com>
2024-04-15 13:35:08 +0200

    fix: fetching data from posts

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: req => !req.url.startsWith('/foundlost'),
-  ignoredRoutes: ['/api/webhook'],
+  publicRoutes: req => !req.url.includes('/foundlost'),
+  ignoredRoutes: ['/api/webhook', '/sign-in'],
 });
 
 export const config = {
