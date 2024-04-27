ab67f9bb9c5be21c3dca897294a311d16e572904
Bernard Kastrati <berni.kastrati@hotmail.com>
2024-04-27 18:03:16 +0200

    trying

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/server/db';
 
+export const dynamic = 'force-dynamic';
 export async function GET(req: Request) {
   try {
     const post = await prisma.lostAndFound.findMany();
-
 
     return NextResponse.json({ post });
   } catch (error) {
