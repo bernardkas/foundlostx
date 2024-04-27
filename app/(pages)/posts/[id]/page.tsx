0a1ac88113f0d568e38264bc21272a33eaf94aa5
Bernard Kastrati <berni.kastrati@hotmail.com>
2024-04-27 15:43:41 +0200

    trying again

--- app/(pages)/posts/[id]/page.tsx
+++ app/(pages)/posts/[id]/page.tsx
@@ -1,10 +1,38 @@
+'use client';
 import AllPosts from '@/components/posts/AllPosts';
 import { Skeleton } from '@/components/ui/skeleton';
+import { LostAndFound } from '@prisma/client';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Posts = () => {
+  const [allPosts, setAllPosts] = useState<LostAndFound[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchPostsData = async () => {
+    const resonse = await axios
+      .get('/api/posts')
+      .then(res => {
+        const data = res.data.post;
+        if (data) {
+          setAllPosts(data);
+          setLoading(false);
+        }
+      })
+      .catch(error => {
+        console.log('Error fetching data', error);
+      });
+
+    return resonse;
+  };
+
+  useEffect(() => {
+    fetchPostsData();
+  });
+
   return (
     <div>
-        <AllPosts />
+      <AllPosts allPosts={allPosts} loading={loading} />
     </div>
   );
 };
