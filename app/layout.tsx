c3ca7855f90e3a05a9f8e48930175aa20a917ac4
Bernard Kastrati <berni.kastrati@hotmail.com>
2024-04-28 18:37:42 +0200

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { auth } from '@clerk/nextjs';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-// import GoogleAnalytics from '@/components/GoogleAnalytics/GoogleAnalytic';
-import { GoogleAnalytics } from '@next/third-parties/google';
+import GoogleAnalytics from '@/components/GoogleAnalytics/GoogleAnalytic';
+// import { GoogleAnalytics } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,11 +27,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS!} />
+        {/* <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS!} /> */}
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
         <body className={inter.className}>
-          {/* {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
-            <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
-          ) : null} */}
           <Navbar userId={userId} />
           {children}
           <Footer />
